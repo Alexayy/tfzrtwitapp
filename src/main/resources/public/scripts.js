a9b7c389cdb4f93cbe3712966db3e8e39a4cc5fa
Nikola Bobinac <nikolabobinac@gmail.com>
2020-08-17 20:07:45 +0200

    Fixed JS for new border styles

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -2,15 +2,18 @@
 		$('.js-nikola-name').on('click', function() {
 		    if (!$('.js-aleksa-links').hasClass("links-hidden"))
 		    {
-		        $('.js-aleksa-links').addClass("links-hidden")
+		        $('.js-aleksa-links').addClass("links-hidden");
+                 $('.js-aleksa-links').removeClass("border-links-aleksa");
 		    }
             if ($('.js-nikola-links').hasClass("links-hidden"))
             {
                 $('.js-nikola-links').removeClass("links-hidden");
+                $('.js-nikola-links').addClass("border-link-nikola");
             }
             else
             {
                  $('.js-nikola-links').addClass("links-hidden");
+                 $('.js-nikola-links').removeClass("border-links-nikola");
             }
          });
 
@@ -18,14 +21,17 @@
 		$('.js-aleksa-name').on('click', function() {
 		    if (!$('.js-nikola-links').hasClass("links-hidden"))
 		    {
-		        $('.js-nikola-links').addClass("links-hidden")
+                $('.js-nikola-links').removeClass("border-links-nikola");
+		        $('.js-nikola-links').addClass("links-hidden");
 		    }
             if ($('.js-aleksa-links').hasClass("links-hidden"))
             {
+                $('.js-aleksa-links').addClass("border-links-aleksa");
                 $('.js-aleksa-links').removeClass("links-hidden");
             }
             else
             {
                  $('.js-aleksa-links').addClass("links-hidden");
+                 $('.js-aleksa-links').removeClass("border-links-aleksa");
             }
          });
