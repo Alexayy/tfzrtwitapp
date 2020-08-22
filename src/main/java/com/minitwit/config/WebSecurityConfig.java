39e484b0575cdcb4916afd9e85c2d11b49b617ed
Nikola Bobinac <Nikolabobinac@gmail.com>
2020-08-22 17:15:43 +0200

    Strict transport test

--- src/main/java/com/minitwit/config/WebSecurityConfig.java
+++ src/main/java/com/minitwit/config/WebSecurityConfig.java
@@ -19,9 +19,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().cacheControl();
         http.headers().contentTypeOptions();
 
+        http.headers().httpStrictTransportSecurity().maxAgeInSeconds(31536000);
+
         RequestMatcher matcher = new AntPathRequestMatcher("/login");
         DelegatingRequestMatcherHeaderWriter headerWriter =
                 new DelegatingRequestMatcherHeaderWriter(matcher,new XFrameOptionsHeaderWriter());
         http.headers().addHeaderWriter(headerWriter);
     }
 }
+
+/*
+<add name="x-ua-compatible" value="IE=Edge" />
+        <add name="feature-policy" value="microphone 'none'; payment 'none'; sync-xhr 'self' https://www.centamin.com/" />
+        <add name="referrer-policy" value="strict-origin-when-cross-origin" />
+        <add name="strict-transport-security" value="max-age=31536000; includeSubDomains" />
+        <add name="x-content-type-options" value="nosniff" />
+        <add name="x-frame-options" value="SAMEORIGIN" />
+        <add name="x-xss-protection" value="1; mode=block" />
+ */
